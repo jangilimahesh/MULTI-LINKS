8a684ab0bd04a4e85677817fe0f8168394fb3ab7
the-piro <thezake0077@gmail.com>
2025-12-13 11:46:00 +0530

    Update __main__.py

--- echobotz/__main__.py
+++ echobotz/__main__.py
@@ -1,5 +1,5 @@
 # ruff: noqa: E402
-
+import os
 from datetime import datetime
 from logging import Formatter
 from asyncio import gather
@@ -30,7 +30,26 @@ async def main():
 
     add_plugs()
 
-    LOGGER.info("All EchoBot Services started")
+    if os.path.isfile(".restartmsg"):
+        try:
+            with open(".restartmsg") as f:
+                chat_id, msg_id = map(int, f.read().splitlines())
+
+            now = datetime.now(timezone(Config.TIMEZONE)).strftime(
+                "%d/%m/%Y %I:%M:%S %p"
+            )
+
+            await EchoBot.bot.edit_message_text(
+                chat_id=chat_id,
+                message_id=msg_id,
+                text=f"<b>Restarted Successfully!</b>\n<code>{now}</code>",
+            )
+
+            os.remove(".restartmsg")
+        except Exception as e:
+            LOGGER.error(f"{e}")
+
+    LOGGER.info("All EchoBot Services Started")
 
     await idle()
 
